2b09baae036cd9c663a3cf51666f75d2d8893fd1
Paco Mastermind <paco@mastermind.ac>
2023-09-13 12:47:08 +0200

    Proyecto inicial con rutas

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,41 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="navbar fixed-top navbar-expand-lg bg-body-secondary">
+              <div className="container-fluid">
+                  <a className="navbar-brand d-flex  align-items-center" href="#">
+                      <img src="./img/logo.png" alt="Logo" height="35" class="d-inline-block align-text-top"/>
+                      <span className="fs-2 ps-3 fw-bold">ElecScoot</span>
+                  </a>
+                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                      <li className="nav-item">
+                        <Link href="/" className="nav-link active">Home</Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link href="/scooter" className="nav-link">Scooter</Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link href="/profile" className="nav-link">Profile</Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="form-check form-switch">
+                      <input className="form-check-input" 
+                      type="checkbox" role="switch" 
+                      id="flexSwitchCheckDefault" />
+                      <label className="form-check-label" for="flexSwitchCheckDefault">Tema oscuro</label>
+                  </div>
+              </div>
+          </nav>
+          <div className="container contPrincipal pt-3">
+            {children}
+          </div>
+        </body>
     </html>
   )
 }
---
+++ app/scooter/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>SCOOTER List</h1>
+  }
